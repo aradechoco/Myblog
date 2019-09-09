5af4e5ce989b619f60bc3a42b06f2841a7040aa8
aradechoco <55019123+aradechoco@users.noreply.github.com>
2019-09-09 17:38:49 +0800

    Update index.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const Footer = () => (
   <footer className="footer">
     © <a href="https://aradechoco.com">aradechoco</a> , Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">Gatsby</a>
+    <a href="https://www.gatsbyjs.org">Gatsby</a>
     
   </footer>
 )
